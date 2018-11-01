02a3636f42dff3008df3fbfcf8861b31900ceace
Mike Burns <maddogmikeb@users.noreply.github.com>
2018-11-01 13:17:36 +1000

    allow to change file

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var qs = (function(a) {
+  if (a == "") return {};
+  var b = {};
+  for (var i = 0; i < a.length; ++i)
+  {
+      var p=a[i].split('=', 2);
+      if (p.length == 1)
+          b[p[0]] = "";
+      else
+          b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+  return b;
+})(window.location.search.substr(1).split('&'));
+
 ;(function () {
   function setUpBoxResizing () {
     function resizeFirstBox (to) {
@@ -133,8 +147,10 @@
         }
         onchange()
       }
-      // var file = 'Example1/example1'
-      var file = 'WMCoachingPlan/version_0_1'
+
+      var file = qs["file"];
+      if (!file) file = 'Example1/example1'
+      //var file = 'WMCoachingPlan/version_0_1'
       $('#definition, #title').on('input', onchange)
       $('#definition').load(file + '.map', onchange)
       $('#additionalStyle')
